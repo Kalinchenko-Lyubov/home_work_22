64d5177f458d54cec46447e4249b73242d21176e
Luybov <kalna.lyubov2017@yandex.ru>
2026-05-21 14:08:21 +0300

    реализованы функции

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def contacts(request):
+    context = {}
+
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+
+        print('Новая заявка:')
+        print(f'Имя: {name}')
+        print(f'Телефон: {phone}')
+        print(f'Сообщение: {message}')
+
+        context['success'] = 'Сообщение успешно отправлено!'
+
+    return render(request, 'contacts.html', context)
