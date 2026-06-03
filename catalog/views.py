aafd262b74e49f17df657e78379147082bb29971
Luybov <kalna.lyubov2017@yandex.ru>
2026-06-03 16:27:10 +0300

    реализовано задание по дз 24

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from catalog.models import Product
 
 
 def home(request):
-    return render(request, "home.html")
+    products = Product.objects.all()
 
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'home.html', context)
 
 def contacts(request):
     context = {}
@@ -21,3 +27,17 @@ def contacts(request):
         context["success"] = "Сообщение успешно отправлено!"
 
     return render(request, "contacts.html", context)
+
+
+def product_detail(request, pk):
+
+    product = get_object_or_404(
+        Product,
+        pk=pk
+    )
+
+    return render(
+        request,
+        'product_detail.html',
+        {'product': product}
+    )
